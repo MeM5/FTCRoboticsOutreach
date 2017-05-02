7270c291d1fc98b4476ea47dd70a6dc25e61bf93
morzack <john_kesler@caryacademy.org>
2017-05-02 09:56:22 -0400

    Finished the MotorPair class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/MotorPair.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/MotorPair.java
@@ -19,4 +19,28 @@ public class MotorPair {
         motor1.active = !motor1.active;
         motor2.active = !motor2.active;
     }
+
+    public void turnAllOn() {
+        motor1.active = true;
+        motor2.active = true;
+    }
+
+    public void turnAllOff() {
+        motor1.active = false;
+        motor2.active = false;
+    }
+
+    public void setAllVelocity(float velocity) {
+        motor1.velocity = velocity;
+        motor2.velocity = velocity;
+    }
+
+    public void reverseMotorVelocity(boolean a, boolean b) {
+        if (a) {
+            motor1.reverse();
+        }
+        if (b) {
+            motor2.reverse();
+        }
+    }
 }
